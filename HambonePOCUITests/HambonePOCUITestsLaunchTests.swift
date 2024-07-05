7964aeae2e8f8ae5d486f949b800b7a6dd2be3b2
Charles Kincy <cpk1971@gmail.com>
2024-07-05 11:09:22 -0700

    Initial Commit

---
+++ HambonePOCUITests/HambonePOCUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HambonePOCUITestsLaunchTests.swift
+//  HambonePOCUITests
+//
+//  Created by Charles Kincy on 2024-07-05.
+//
+
+import XCTest
+
+final class HambonePOCUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
